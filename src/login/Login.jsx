37355960f5ffcedcb6d82ac8a92d19a063495e23
Franco <diegoantoniolopezruelas@gmail.com>
2022-10-25 10:14:58 -0500

    Se subio un archivo de ejemplo llamado login

---
+++ src/login/Login.jsx
@@ -0,0 +1,11 @@
+import st from "./login.module.css";
+function Login() {
+  return (
+    <>
+      <h3>Hola Mundo</h3>
+      <div className={st.p}>hola</div>
+    </>
+  );
+}
+
+export default Login;
