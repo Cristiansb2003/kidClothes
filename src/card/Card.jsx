7c6b68edb8c0eea96e8b47354666187256c65ce3
Oziel <angelozzy1@gmail.com>
2022-10-28 10:31:28 -0500

    card lista

--- src/card/Card.jsx
+++ src/card/Card.jsx
@@ -1,6 +1,65 @@
+import './card.css'
 function Card(){
-    return(
-        <h1>HOLA</h1>
-    );
+    return(<>
+    
+    <table>
+        <td>
+        <div className="externo">
+            <div className='superior'></div>
+            <div className='inferior'> </div>
+
+            <div className='gris'>
+
+                <img src="\src\imagenes\redim_toddlerb.jpg" alt="" className='imagen'/>
+
+            </div>
+
+            <div className='textos'>
+                <p>Nombre</p>
+                <p>Precio</p>
+                <a href='https://www.google.com' className='links'><img src="\src\imagenes\carrito.png" alt="" className='imagen2'/></a>
+            </div>
+        </div>
+        </td>
+
+        <td>
+        <div className="externo">
+            <div className='superior'></div>
+            <div className='inferior'> </div>
+
+            <div className='gris'>
+
+                <img src="\src\imagenes\redim_toddlerb.jpg" alt="" className='imagen'/>
+
+            </div>
+
+            <div className='textos'>
+                <p>Nombre</p>
+                <p>Precio</p>
+                <a href='https://www.google.com' className='links'><img src="\src\imagenes\carrito.png" alt="" className='imagen2'/></a>
+            </div>
+        </div>
+        </td>
+        <td>
+        <div className="externo">
+            <div className='superior'></div>
+            <div className='inferior'> </div>
+
+            <div className='gris'>
+
+                <img src="\src\imagenes\redim_toddlerb.jpg" alt="" className='imagen'/>
+
+            </div>
+
+            <div className='textos'>
+                <p>Nombre</p>
+                <p>Precio</p>
+                <a href='https://www.google.com' className='links'><img src="\src\imagenes\carrito.png" alt="" className='imagen2'/></a>
+            </div>
+        </div>
+        </td>
+    </table>
+
+        </>);
 }
 export default Card
