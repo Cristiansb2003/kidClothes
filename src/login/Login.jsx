8e290f3ac2098209c557954e3e5ba9f88b0f854e
Franco <diegoantoniolopezruelas@gmail.com>
2022-10-31 09:39:05 -0600

    Se agregaron textbox y un boton

--- src/login/Login.jsx
+++ src/login/Login.jsx
@@ -2,8 +2,20 @@ import st from "./login.module.css";
 function Login() {
   return (
     <>
-      <h3>Hola Mundo</h3>
-      <div className={st.p}>hola</div>
+      <div className={st.formulario}>
+        <h1>Login</h1>
+        <form action="" method="post">
+          <div className={st.fomrulario-col}>
+            <p>Usuario</p>
+            <input type="text" name="user" />
+          </div>
+          <div className={st.formulario-col}>
+            <p>Contraseña</p>
+            <input type="password" name="pass" />
+          </div>
+          <button type="submit" className={st.formulario-button}>Iniciar sesión</button>
+        </form>
+      </div>
     </>
   );
 }
