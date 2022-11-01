94004ead174629f7f4ed69717f29eca260b4acb9
Franco <diegoantoniolopezruelas@gmail.com>
2022-10-31 21:16:37 -0600

    Se terminó el registro de usuarios con localstorage, queda pendiente actualizar la interfaz para que diga que es registro y hacer un login de verdad

--- src/login/Login.jsx
+++ src/login/Login.jsx
@@ -1,20 +1,60 @@
+import { useState } from "react";
 import st from "./login.module.css";
-function Login() {
+import Test from "./Test";
+
+const Login = () => {
+
+  // State para consumir el valor del usuario
+  const [user, setUser] = useState('');
+  // State para consumir el valor de la contraseña
+  const [pass, setPass] = useState('');
+  
+  // State para actualizar el usuario y contraseña automaticamente
+  const [savedData, setSavedData] = useState(false);
+
+  // Esta función almacenará cambios en el input de usuario
+  const cambioEntradaUser = (e) => {
+    const text = e.target.value
+    setUser(text);
+  }
+
+  // Esta función almacenará cambios en el input de constraseña
+  const cambioEntradaPass = (e) => {
+    const text = e.target.value
+    setPass(text);
+  }
+
+  // Esta función es para guardar los datos
+  const saveData = () => {
+    localStorage.setItem("user", user);
+    localStorage.setItem("password", pass);
+
+    alert('Esto era el registro todo el rato XD');
+    setSavedData(true);
+  }
+
   return (
-    <>
-      <div className={st.formulario}>
-        <h1>Login</h1>
-        <form action="#" method="post">
-          <p>Usuario</p>
-          <input type="text" name="user" />
-          <p>Contraseña</p>
-          <input type="password" name="pass" />
-          <br />
-          <button type="submit" className={st.formulario_button}>Iniciar sesión</button>
-        </form>
+    <div className={st.formulario}>
+      <h1>Login</h1>
+      <div>
+        Usuario <br />
+        <input 
+          type="text" name="user" placeholder="Ingresa tu usuario"
+          onChange = {cambioEntradaUser}
+        />
+        Contraseña <br />
+        <input 
+          type="password" name="pass" placeholder="Ingresa tu contraseña" 
+          onChange={cambioEntradaPass}
+        />
+        <br />
+        <button onClick={saveData} >Iniciar sesión</button>
       </div>
-    </>
-  );
+
+      { (!!savedData) && <Test />}
+
+    </div>
+  )
 }
 
 export default Login;
