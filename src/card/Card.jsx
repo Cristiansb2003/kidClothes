2ad3fff916fb5a0adc4ca6fdd60676f84ff13523
Oziel <angelozzy1@gmail.com>
2022-11-06 16:06:20 -0600

    AVANCES QS Y FOOTER

--- src/card/Card.jsx
+++ src/card/Card.jsx
@@ -2,7 +2,7 @@ import './card.css'
 function Card(){
     return(<>
     
-    <table>
+    <table className='tablagen'>
         <td>
         <div className="externo">
             <div className='superior'></div>
@@ -10,18 +10,21 @@ function Card(){
 
             <div className='gris'>
 
-                <img src="\src\imagenes\redim_toddlerb.jpg" alt="" className='imagen'/>
+                <img src="\src\imagenes\conjcel.jpg" alt="" className='imagen'/>
 
             </div>
 
             <div className='textos'>
-                <p>Nombre</p>
-                <p>Precio</p>
-                <a href='https://www.google.com' className='links'><img src="\src\imagenes\carrito.png" alt="" className='imagen2'/></a>
+            <p className='txt'>Nombre</p>
+            <p className='txt'>Precio</p>
+            </div>
+
+            <div className='comprar'>
+            <img src="\src\imagenes\carrocompras.png" alt="" className='imagen2'/>
             </div>
         </div>
         </td>
-
+        
         <td>
         <div className="externo">
             <div className='superior'></div>
@@ -34,9 +37,12 @@ function Card(){
             </div>
 
             <div className='textos'>
-                <p>Nombre</p>
-                <p>Precio</p>
-                <a href='https://www.google.com' className='links'><img src="\src\imagenes\carrito.png" alt="" className='imagen2'/></a>
+            <p className='txt'>Nombre</p>
+            <p className='txt'>Precio</p>
+            </div>
+
+            <div className='comprar'>
+            <img src="\src\imagenes\carrocompras.png" alt="" className='imagen2'/>
             </div>
         </div>
         </td>
@@ -47,14 +53,17 @@ function Card(){
 
             <div className='gris'>
 
-                <img src="\src\imagenes\redim_toddlerb.jpg" alt="" className='imagen'/>
+                <img src="\src\imagenes\redim_conjnar.jpg" alt="" className='imagen'/>
 
             </div>
 
             <div className='textos'>
-                <p>Nombre</p>
-                <p>Precio</p>
-                <a href='https://www.google.com' className='links'><img src="\src\imagenes\carrito.png" alt="" className='imagen2'/></a>
+            <p className='txt'>Nombre</p>
+            <p className='txt'>Precio</p>
+            </div>
+
+            <div className='comprar'>
+            <img src="\src\imagenes\carrocompras.png" alt="" className='imagen2'/>
             </div>
         </div>
         </td>
--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import Card from './card/Card'
+import Ffooter from './footer/Footer'
 import './index.css'
+import Qsomos from './qsomos/qsomos'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     
     <Card/>
+    <Card/>
+    <Qsomos/>
+    <Ffooter/>
+    
+
+    
   </React.StrictMode>
 )
